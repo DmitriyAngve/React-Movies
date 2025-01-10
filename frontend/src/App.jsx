185193429592711d08395eccefdcbbfa8bbeb946
DmitriyAngve <kirirch129@gmail.com>
2025-01-10 14:51:21 +0300

    #

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Text display="hello1" />
-      <Text display="hello" />
-    </>
-  );
-}
-function Text({ display }) {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
