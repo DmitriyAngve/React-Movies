261b8dcb4af3a9307ed1ef79133115f92cb217d2
DmitriyAngve <kirirch129@gmail.com>
2025-01-13 16:45:22 +0300

    #

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import MovieCard from "./components/MovieCard";
 
 function App() {
-  return <></>;
+  const movieNumber = 1;
+
+  return (
+    <>
+      {movieNumber === 1 && (
+        <MovieCard movie={{ title: "Film 1", release_date: "2026" }} />
+      )}
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,15 +1,30 @@
+import PropTypes from "prop-types";
+
 function MovieCard({ movie }) {
   function onFavoriteClick() {}
+
   return (
     <div className="movie-card">
       <div className="movie-poster">
         <img src={movie.url} alt={movie.title} />
         <div className="movie-overlay">
-          <button className="favorite-btn" onClick={onFavoriteClick}></button>
+          <button className="favorite-btn" onClick={onFavoriteClick}>
+            Text
+          </button>
         </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
       </div>
     </div>
   );
 }
+
+MovieCard.propsType = {
+  movie: PropTypes.shape({
+    release_date: PropTypes.shape.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
