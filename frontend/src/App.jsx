f344e9083d0fc7f0b9ebc3d5673c23c670878db6
DmitriyAngve <kirirch129@gmail.com>
2025-01-20 16:06:14 +0300

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
+
+import Favorite from "./pages/Favorites";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div>
-        <Home />
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorite />} />
+          </Routes>
+        </main>
       </div>
     </>
   );
