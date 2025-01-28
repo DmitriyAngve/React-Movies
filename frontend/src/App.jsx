d52a403853f939c86d55215e9502afd2580eecca
DmitriyAngve <kirirch129@gmail.com>
2025-01-28 12:19:53 +0300

    feat: add favorites

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,20 @@ import "./css/App.css";
 import Favorite from "./pages/Favorites";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-
+import { MovieProvider } from "./context/MovieContext";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div>
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favorites" element={<Favorite />} />
-          </Routes>
-        </main>
-      </div>
-    </>
+    <MovieProvider>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorite />} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
 
--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,7 +1,15 @@
 import "../css/MovieCard.css";
+import { useMovieContext } from "../context/MovieContext";
 
 function MovieCard({ movie }) {
-  function onFavoriteClick() {}
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
+  function onFavoriteClick(e) {
+    e.preventDefault();
+    if (favorite) removeFromFavorites(movie.id);
+    else addToFavorites(movie);
+  }
 
   return (
     <div className="movie-card">
@@ -11,8 +19,11 @@ function MovieCard({ movie }) {
           alt={movie.title}
         />
         <div className="movie-overlay">
-          <button className="favorite-btn" onClick={onFavoriteClick}>
-            Text
+          <button
+            className={`favorite-btn ${favorite ? "active" : ""}`}
+            onClick={onFavoriteClick}
+          >
+            ♥
           </button>
         </div>
       </div>
